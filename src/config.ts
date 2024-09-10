5a198998ddddff81f9483b26ae40e189129228fa
Williams Pizarro <will.pizarro.wp@gmail.com>
2024-09-10 18:21:27 -0300

    se modifica archivo config y local-env

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const configuration = {
-    username: process.env.USERNAME ?? "Desconocido",
+    username: process.env.USERNAME ?? "Desconocido WI",
     port: process.env.PORT ?? 3001,
     apiKey: process.env.API_KEY ?? "Desconocida"
 }
