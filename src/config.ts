5795de55196a8e8e85a4448687e20e17ea73e35c
Williams Pizarro <will.pizarro.wp@gmail.com>
2024-09-11 23:21:56 -0300

    se agrega test de prueba para config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export const configuration = {
     username: process.env.USERNAME ?? "Williams",
-    port: process.env.PORT ?? 3001,
+    port: process.env.PORT ? parseInt(process.env.PORT, 10) : 3001, // Asegurando que port sea un número
     apiKey: process.env.API_KEY ?? "Desconocida"
-}
+};
+
